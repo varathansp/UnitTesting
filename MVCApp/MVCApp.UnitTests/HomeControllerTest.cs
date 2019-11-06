03d37c38103c36bdd5f2960115af7101101886a1
Varathan <varathansuyambu95@gmail.com>
2019-11-06 14:14:48 +0530

    unit test on mvc added

---
+++ MVCApp/MVCApp.UnitTests/HomeControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCApp.Controllers;
+using System.Web.Mvc;
+
+
+
+namespace MVCApp.UnitTests
+{
+    //refer system.web.mvc
+    //refer system.web.webpages
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index_IdValueMoreThan5_ReturnsBase()
+        {
+            HomeController homeController = new HomeController();
+            ActionResult actionResult = homeController.Index(6);
+            Assert.IsInstanceOfType(actionResult, typeof(RedirectToRouteResult));
+            RedirectToRouteResult redirectToRouteResult = actionResult as RedirectToRouteResult;
+            Assert.AreEqual(redirectToRouteResult.RouteValues["action"], "Base");
+        }
+        [TestMethod]
+        public void Index_IdValueLessThan5_ReturnsIndex()
+        {
+            HomeController homeController = new HomeController();
+            ActionResult result = homeController.Index(1);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+
+            ViewResult view = result as ViewResult;
+            Assert.AreEqual("Index", view.ViewName);
+            
+            //ViewResult vResult = result as ViewResult;
+            //if (vResult != null)
+            //{
+            //    Assert.IsInstanceOfType(vResult.Model, typeof());
+            //    YourModelType model = vResult.Model as YourModelType;
+            //    if (model != null)
+            //    {
+            //        //...
+            //    }
+            //}
+
+            //view.ViewData.Model
+        }
+
+    }
+}
---
+++ MVCApp/MVCApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(int id)
+        {
+            if (id > 5)
+            {
+                return RedirectToAction("Base");
+            }
+            ViewBag.ID = id;
+            return View("Index");
+        }
+        public ActionResult Base()
+        {
+         
+            return View();
+        }
+
+      
+    }
+}
